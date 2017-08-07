50150e5f5ce914b53bea56bb30b18698d0cbcea8
aldy120 <aldy120@yahoo.com.tw>
2017-08-07 10:05:01 +0800

    add get started

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # pig-rain10
 自動發送雨量警報
-
+# Get started
+```
+git clone https://github.com/aldy120/pig-rain10.git
+cd pig-rain10
+npm install
+touch routes/mailset.js
+npm start
+```
 # data source
 - 中央氣象局
   - [十分鐘雨量資料](http://www.cwb.gov.tw/V7/observe/rainfall/A136.htm)
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-npm install -y
-mv routes/mailset-sample.js routes/mailset.js
-npm run production
+curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+yum -y install nodejs git
+git clone https://github.com/aldy120/pig-rain10.git
+cd pig-rain10
+npm install
+touch routes/mailset.js
+npm start
